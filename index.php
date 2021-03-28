5ee4ce196e9c17454a1fe4f3198ddcb46aaf211e
robalb <alberto.ventafridda@gmail.com>
2021-03-28 15:08:38 +0200

    initial files

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+ header("location: menu.php");
+ die();
+ 
+ ?>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" lang="it">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" lang="it">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" lang="it">
+<![endif]-->
+<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
+<html lang="it">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name='robots' content='noindex,nofollow' />
+    <meta name="language" content="it">
+    <title>premiata libreria della nonna</title>
+    <meta name="description" content="Premiata libreria della nonna" />
+    <link rel="canonical" href="https://halb.it/pln/"/>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="keywords" content="pln" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+
+  </body>
+</html>
+
---
+++ roadmap.md
@@ -0,0 +1,17 @@
+premiata libreria franca
+
+tabulator
+con 18n
+http://tabulator.info/docs/4.9/localize
+semantic ui theme
+/dist/css/semantic-ui/tabulator_semantic-ui.min.css
+
+custom add row bar,
+and editable features
+http://tabulator.info/docs/4.9/update#row
+all interfacing with callbacks with the updatedata backend apis
+
+undo redo
+http://tabulator.info/docs/4.9/history
+
+https://codepen.io/dragon-gt/pen/JveVmm
